239cea79af91b3c678ec6b431b9a1e0b0cb9b637
Alekseev Artem <fexolm@gmail.com>
2019-03-23 23:54:54 +0300

    more strict tests for symbol map

--- src/parser/test/helpers.rs
+++ src/parser/test/helpers.rs
@@ -4,9 +4,12 @@ use super::ast;
 use super::ast::helpers::*;
 
 pub fn expect_var(name: &str, scope: NodeId, arena: &Arena<ast::Scope>) -> bool {
-    let sym = scope.try_lookup(&name.to_string(), arena).unwrap();
-    if let ast::Symbol::Variable(ref var) = *sym {
-        var.name == name.to_string()
+    if let Ok(sym) = scope.try_lookup(&name.to_string(), arena) {
+        if let ast::Symbol::Variable(ref var) = *sym {
+            var.name == name.to_string()
+        } else {
+            false
+        }
     } else {
         false
     }
--- src/parser/test/symbol_map.rs
+++ src/parser/test/symbol_map.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 use super::helpers;
 use super::parser;
+use super::ast::helpers::StmtExtensions;
 
 #[test]
 fn test_symbol_map() {
@@ -11,8 +12,16 @@ fn test_symbol_map() {
     for f in ast.functions {
         if f.name == "bar" {
             let scope = f.body.scope;
-            assert!(helpers::expect_var("c", scope, &arena));
             assert!(helpers::expect_var("a", scope, &arena));
+            assert!(helpers::expect_var("b", scope, &arena));
+            assert!(helpers::expect_var("c", scope, &arena));
+            assert!(!helpers::expect_var("foo", scope, &arena));
+
+            let block = f.body.list[1].as_block().expect("Second statement must be block");
+            assert!(helpers::expect_var("a", block.scope, &arena));
+            assert!(helpers::expect_var("b", block.scope, &arena));
+            assert!(helpers::expect_var("c", block.scope, &arena));
+            assert!(helpers::expect_var("foo", block.scope, &arena));
         }
     }
 }
