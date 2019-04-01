7d09ec4adce38b8b6fd79d8e07fa1eb8a9637e4e
Alekseev Artem <fexolm@gmail.com>
2019-04-01 23:23:08 +0300

    add ssa benchmark

--- benches/create_ast.rs
+++ benches/create_ast.rs
@@ -3,6 +3,7 @@ extern crate criterion;
 extern crate jfec;
 
 use criterion::Criterion;
+use jfec::codegen::ssa::emmiter;
 use jfec::parser;
 use std::fs;
 
@@ -14,5 +15,15 @@ fn bench_create_ast(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(benches, bench_create_ast);
+fn bench_create_ssa(c: &mut Criterion) {
+    let file = fs::read_to_string("testdata/bench.jfec").expect("cannot read file");
+    let ast = parser::create_ast(&file).expect("cannot create ast");
+
+    c.bench_function("create ast", |b| b.iter(|| {
+        emmiter::to_ssa(&ast)
+    }));
+}
+
+criterion_group!(benches, bench_create_ast, bench_create_ssa);
+
 criterion_main!(benches);
